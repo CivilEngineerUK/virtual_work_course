f3d381a378e2a81cadae4cefd8b974c5bd7270ba
CivilEngineerUK <michael.rustell25@gmail.com>
2019-05-06 18:06:28 +0100

    Create exc_01_04.R

---
+++ exercises/exc_01_04.R
@@ -0,0 +1,17 @@
+library(ggplot2)
+
+# applied point load
+P <- 10 # kN
+
+# length of the member
+L <- ____ # m
+
+# max bending moment
+M <- ____ * ____
+
+# bending moment along beam
+BM <- data.frame(L = c(0, L),
+                 M = c(M, 0))
+
+# plot bending moment
+plot(BM, type = 'lwd')
