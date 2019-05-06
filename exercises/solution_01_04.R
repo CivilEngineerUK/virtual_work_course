a3c1b48e025d4afc96d1c92e0832f83b096f2480
CivilEngineerUK <michael.rustell25@gmail.com>
2019-05-06 18:14:01 +0100

    Create solution_01_04.R

---
+++ exercises/solution_01_04.R
@@ -0,0 +1,15 @@
+# applied point load
+P <- 10 # kN
+
+# length of the member
+L <- 5 # m
+
+# max bending moment
+M <- P * L
+
+# bending moment along beam
+BM <- data.frame(L = c(0, L),
+                 M = c(M, 0))
+
+# plot bending moment
+plot(BM, type = 'lwd')
